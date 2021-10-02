848d2099d97819e00143fe9916f28e84b4cc6db2
CeaganP <43758262+CeaganP@users.noreply.github.com>
2021-10-02 15:38:33 -0400

    Update geoPi.py

--- geoPi.py
+++ geoPi.py
@@ -18,33 +18,22 @@ def plot(img, title):
     plt.axis('off')
     plt.show()
 
+def getSatImage(zoom, level, lat, long) :
+    point = Point.from_latitude_longitude(lat, long)
+    print('Pixels: ', point.pixels(zoom=zoom))  # Pixels:  (34430592, 49899136)
+    print('Lat/Lon: ', point.latitude_longitude)  # Lat/Lon:  (41.84987190947754, -87.64995574951166)
 
-#meter_x, meter_y = -9757148.442088600, 5138517.444985110 # meters in Spherical Mercator EPSG:900913
-#point = Point.from_meters(meter_x=meter_x, meter_y=meter_y)
+    tms_x, tms_y = 134494, 329369
+    tile = Tile.from_tms(tms_x=tms_x, tms_y=tms_y, zoom=zoom)  # Tile Map Service (TMS) X Y and zoom
 
-zoom = 19
-#FEED LAT,LONG
-point = Point.from_latitude_longitude(43.2557, -79.8711)
+    row = int(((90 - point.latitude) * (2 ** level)) // 288)
+    col = int(((180 + point.longitude) * (2 ** level)) // 288)
 
-print('Pixels: ', point.pixels(zoom=zoom))  # Pixels:  (34430592, 49899136)
-print('Lat/Lon: ', point.latitude_longitude)  # Lat/Lon:  (41.84987190947754, -87.64995574951166)
-
-level, tms_x, tms_y, zoom = 7, 134494, 329369, 19
-tile = Tile.from_tms(tms_x=tms_x, tms_y=tms_y, zoom=zoom)  # Tile Map Service (TMS) X Y and zoom
-
-print('QuadTree: ', tile.quad_tree)  # QuadTree:  0302222310303211330
-print('Google: ', tile.google)  # Google:  (134494, 194918)
-
-row = int(((90 - point.latitude) * (2 ** level)) // 288)
-col = int(((180 + point.longitude) * (2 ** level)) // 288)
-
-url = 'https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/MODIS_Terra_CorrectedReflectance_TrueColor/default/2016-09-03/250m/'
-image_url = url + str(level) + "/" + str(row) + "/" + str(col) + ".jpg"
-
-print('Website: ', image_url)
+    url = 'https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/MODIS_Terra_CorrectedReflectance_TrueColor/default/2016-09-03/250m/'
+    return url + str(level) + "/" + str(row) + "/" + str(col) + ".jpg"
 
 fileName = "satImage.jpg"
-img_data = requests.get(image_url).content
+img_data = requests.get(getSatImage(19, 7, 43.2557, -79.8711)).content
 with open(fileName, 'wb') as handler:
     handler.write(img_data)
 
